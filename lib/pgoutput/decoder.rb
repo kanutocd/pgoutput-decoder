47dce9e8debf0d7e48dd34c870b1f410457ed809
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-05-31 17:28:42 +0800

    chore: initialize pgoutput-decoder gem structure

---
+++ lib/pgoutput/decoder.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "decoder/version"
+
+module Pgoutput
+  module Decoder
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pgoutput/decoder/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Pgoutput
+  module Decoder
+    VERSION = "0.1.0"
+  end
+end
